e001f16742e1b49f51ce01c3669518f741267276
as-budi <agungsetiabudi@ub.ac.id>
2022-10-23 21:29:34 +0700

    Add dashboard page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./components/login";
 import Register from "./components/register";
+import Dashboard from "./components/dashboard";
+import Navbar from "./components/navbar";
 
 
 function App() {
      return (
           <BrowserRouter>
                <Routes>
-                    <Route path="/" element={<Login />} />
+                    <Route exact path="/" element={<Login />} />
                     <Route path="/register" element={<Register />} />
+                    <Route path="/dashboard" element={<><Navbar /><Dashboard /></>} />
                </Routes>
           </BrowserRouter>
      );
